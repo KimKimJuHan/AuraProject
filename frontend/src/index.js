5c72e76d61c8c7e9bde19d33c30d2ac304b16cc0
KimKimJuHan <wngks37@naver.com>
2025-12-09 17:14:40 +0900

    251209업데이트

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
+// frontend/src/MainPage.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Link } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function MainPage({ region, user }) {
+  return (
+    <div style={{ padding: '20px', color: '#fff' }}>
+      <h1>메인 페이지</h1>
+      <p>환영합니다! {user ? `${user.username}님` : '로그인해주세요.'}</p>
+      
+      {/* 여기에 원래 있던 배너나 게임 리스트 컴포넌트를 넣으셔야 합니다 */}
+      <div style={{ marginTop: '20px' }}>
+        <p>추천 게임을 확인해보세요.</p>
+        <Link to="/search" style={{ color: '#E50914' }}>게임 검색하러 가기 &gt;</Link>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
